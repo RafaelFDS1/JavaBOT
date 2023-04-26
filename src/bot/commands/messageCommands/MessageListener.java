aa432250d782e1276455367c76f015306df42ca0
Rafael Ferraz de Santana <rafael_fds@outlook.com>
2023-04-26 14:30:21 -0300

    arrumando a casa

--- src/bot/commands/messageCommands/MessageListener.java
+++ src/bot/commands/messageCommands/MessageListener.java
@@ -1,20 +1,20 @@
 package bot.commands.messageCommands;
 
-import bot.commands.eventsResponse.EventEmbedResponses;
+import bot.commands.messageRecievedResponse.GuildMessageResponse;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class MessageListener extends ListenerAdapter {
+import static bot.Main.prefix;
 
-  private static String prefix;
+public class MessageListener extends ListenerAdapter {
 
   public MessageListener(){}
   @Override
   public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-    EventEmbedResponses events = new EventEmbedResponses(event);
+    GuildMessageResponse events = new GuildMessageResponse(event);
     switch(event.getMessage().getContentDisplay()){
       case prefix + "github":
         events.createGitHubEmbed();
